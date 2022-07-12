e9ffcaede07cfb057454eb0cbe078db85291ba7c
Pedro Elias Costa Santos <54581042+PedroEliasCS@users.noreply.github.com>
2022-07-12 20:39:30 -0300

    Fim de configuração

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,47 @@
-import './util/module-alias';
+import "./util/module-alias";
+// importa o alias
+// que mais nada mais é que um
+// encurtador de dir
+// utilizando @src ou @test
 
-import {
-    Server
-} from '@overnightjs/core';
-import bodyParser from 'body-parser';
+import { Server } from "@overnightjs/core";
+import bodyParser from "body-parser";
 
-import {
-    ForecastController
-} from './controllers/forecast';
-import { Application } from 'express';
+import { ForecastController } from "./controllers/forecast";
+import { Application } from "express";
 
 export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    // constructor do setup de configuração do Server
 
-    constructor(private port = 3000) {
-        super();
-    };
+    super();
+  }
 
-    public init(): void {
-        this.setupExpress();
-        this.setupControllers();
-    };
+  public init(): void {
+    // function de iniciação
+    // iniciando os metodos necessarios para
+    // o funcionando do sistema
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-    };
+  private setupExpress(): void {
+    // é feito a configuração do sistema
+    this.app.use(bodyParser.json());
+    // com o uso do bodyParser instancia o metodo de
+    // "conversação" com o e respostas do sistema como json
+  }
 
-    private setupControllers(): void {
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
-    };
+  private setupControllers(): void {
+    // é feito a chamada dos controllers
+    // e a adição deles no this
+    // para assim ser feito as chamadas
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    public getApp(): Application{
-        return this.app;
-    };
-};
+  public getApp(): Application {
+    // retornando o app
+    return this.app;
+  }
+}
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,27 +1,14 @@
 import { SetupServer } from "@src/server";
 import supertest from "supertest";
 
- beforeAll(() => {
-    const server = new SetupServer();
-    server.init();
-    global.testRequest = supertest(server.getApp());
- });
-
-// const {
-//     resolve
-// } = require("path");
-// const root = resolve(__dirname, "..");
-// // busca o diretorio padrão do sistema
-// const rootConfig = require(`${root}/jest.config.js`);
-// // busca a configuração global dos testes
-
-// module.exports = {
-//     ...rootConfig,
-//     ...{
-//         rootDir: root,
-//         display: "end2end-tests",
-//         // essa categoria de testes tera esse nome 
-//         setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
-//         testMatch: ["<rootDir>/test/**/*.test.ts"]
-//     },
-// };
+beforeAll(() => {
+  // a chamada antes de tudo acontecer
+  const server = new SetupServer();
+  // cria um novo OBJ setupServer
+  // que é a configuração do servidor
+  server.init();
+  // inicia o servidor
+  global.testRequest = supertest(server.getApp());
+  // coloca valor no testRequest
+  // sendo ele o app() do servidor
+});
