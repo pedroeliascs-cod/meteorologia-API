5fa6628684b4c1db1a3aa09c830439edac634ed5
Pedro Elias Costa Santos <54581042+PedroEliasCS@users.noreply.github.com>
2022-07-15 16:08:44 -0300

    retornando os dados já padrões

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -17,17 +17,32 @@ export interface Beach {
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
+export interface TimeForcast {
+  time: string;
+  forecast: BeachForecast[];
+}
+
 export class Forecast {
+  // classe para processameto de dados
   constructor(protected stormGlass = new StormGlass()) {}
+  // classe pode receber um stormGlass
+  // mas caso não seja passado instancia um novo stormGlass
 
   public async processForecastForBeaches(
+    // faz o processamento que retorna da api
     beaches: Beach[]
-  ): Promise<BeachForecast[]> {
+  ): Promise<TimeForcast[]> {
+    
     const pointsWithCorrectSources: BeachForecast[] = [];
+    // o array que recebera os valores processados pelo client
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-      // 
+      // points é a variavel retornada pela 
+
       const enrichedBeachData = points.map((e) => ({
+        // e é um obj de informações daquela praia
+        // ...{},{a: b, d: c}, ...e
+        // é um jeito de mesclar os objetos
         ...{},
         ...{
           lat: beach.lat,
@@ -40,6 +55,24 @@ export class Forecast {
       }));
       pointsWithCorrectSources.push(...enrichedBeachData);
     }
-    return pointsWithCorrectSources;
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
+
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForcast[] {
+    const mapForecastByTime: TimeForcast[] = [];  
+
+    for (const point of forecast) {
+      const timePoint = mapForecastByTime.find((f) => f.time === point.time);
+
+      if(timePoint){
+        timePoint.forecast.push(point);
+      }else{
+        mapForecastByTime.push({
+          time: point.time,
+          forecast: [point]
+        })
+      }
+    }
+    return mapForecastByTime;
   }
 }
